0a46eab5ebd6e3bf91e1ca1c6bc6c8462bce08b7
Junaid-Z <jzafar828@gmail.com>
2026-08-02 15:27:24 +0500

    feat(server): add DeviceTransferTable class

---
+++ server/src/features/device-transfer/device-transfer.table.ts
@@ -0,0 +1,45 @@
+import {
+  ImmutableTable,
+  type ImmutableTableParams,
+  type TableColumnParams,
+} from "@shared/lib/database.lib.js";
+
+export const TABLE_NAME = "device_transfer";
+
+export const defaultColumnConfig = {
+  id: { name: "id" },
+  publicId: { name: "public_id" },
+  deviceNumber: {
+    name: "device_number",
+  },
+  toUser: {
+    name: "to_user",
+  },
+  fromUser: {
+    name: "from_user",
+  },
+  completedAt: {
+    name: "completed_at",
+  },
+} satisfies Record<string, TableColumnParams>;
+
+export interface DeviceTransferTableParams extends Omit<
+  ImmutableTableParams<keyof typeof defaultColumnConfig>,
+  "name" | "defaultColumnConfig"
+> {}
+
+export class DeviceTransferTable extends ImmutableTable<
+  keyof typeof defaultColumnConfig
+> {
+  default = new DeviceTransferTable({ schemaName: "public" });
+  constructor(params: DeviceTransferTableParams) {
+    const { alias, columns, schemaName } = params;
+    super({
+      schemaName,
+      name: TABLE_NAME,
+      alias,
+      columns,
+      defaultColumnConfig,
+    });
+  }
+}
