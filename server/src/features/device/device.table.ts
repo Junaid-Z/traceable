d0ae3c2554c84e5446ac15ca7e0cf19f560fdaf6
Junaid-Z <jzafar828@gmail.com>
2026-08-02 15:23:31 +0500

    feat(server): add DeviceTable class

---
+++ server/src/features/device/device.table.ts
@@ -0,0 +1,39 @@
+import {
+  ImmutableTable,
+  type ImmutableTableParams,
+  type TableColumnParams,
+} from "@shared/lib/database.lib.js";
+
+export const TABLE_NAME = "device";
+
+export const defaultColumnConfig = {
+  deviceNumber: {
+    name: "device_number",
+  },
+  deviceDepartment: {
+    name: "device_department",
+  },
+} satisfies Record<string, TableColumnParams>;
+
+export interface DeviceTableParams extends Omit<
+  ImmutableTableParams<keyof typeof defaultColumnConfig>,
+  "name" | "defaultColumnConfig"
+> {}
+
+export class DeviceTable extends ImmutableTable<
+  keyof typeof defaultColumnConfig
+> {
+  static default = new DeviceTable({
+    schemaName: "public",
+  });
+  constructor(params: DeviceTableParams) {
+    const { alias, columns, schemaName } = params;
+    super({
+      alias,
+      columns,
+      schemaName,
+      name: TABLE_NAME,
+      defaultColumnConfig,
+    });
+  }
+}
