fa540b5f0c178d81ed0ccfd2fb254a714cef95f7
Junaid-Z <jzafar828@gmail.com>
2026-08-08 21:46:13 +0500

    chore(server): remove migrations from build output

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+  entry: ["src/**/*.ts", "!src/**/*.test.ts", "!src/migrations/*"],
   onSuccess: "tsc-alias -p ./tsconfig.build.json",
   format: ["esm"],
   splitting: false,
