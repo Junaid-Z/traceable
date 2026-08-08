eabb6a9ee0e60187595f5251c665125b3dc327a3
Junaid-Z <jzafar828@gmail.com>
2026-08-08 21:35:44 +0500

    feat(server): add support for absolute imports

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+  onSuccess: "tsc-alias -p ./tsconfig.build.json",
   format: ["esm"],
   splitting: false,
   sourcemap: true,
