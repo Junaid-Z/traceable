a4feb36be76a3e23430fd440047a18c5bdab7ad9
Junaid-Z <jzafar828@gmail.com>
2026-08-11 21:27:15 +0500

    chore(server): add second user to add_user seed

--- server/src/seeds/development/20260809112558_add_users.ts
+++ server/src/seeds/development/20260809112558_add_users.ts
@@ -21,6 +21,21 @@ export async function seed(knex: Knex): Promise<void> {
           "street 123, some address, some city",
         [UserTable.default.columns.active.name]: true,
       },
+      {
+        [UserTable.default.columns.id.name]:
+          "00000000-0000-0000-0000-000000000001",
+        [UserTable.default.columns.displayName.name]: "Ahmed",
+        [UserTable.default.columns.username.name]: "ahmed",
+        [UserTable.default.columns.password.name]:
+          "$2a$12$JtvNlDmjT.f0hot5AKfY3uYvDSph.M5lwo2HfvfmI.dLg.m28s/fO", //hash for password123
+        [UserTable.default.columns.nic.name]: "4200012345679",
+        [UserTable.default.columns.contact.name]: "923091234568",
+        [UserTable.default.columns.role.name]: USER_ROLE.ADMIN,
+        [UserTable.default.columns.salary.name]: 150_000,
+        [UserTable.default.columns.address.name]:
+          "street 456, some address, some city",
+        [UserTable.default.columns.active.name]: true,
+      },
     ])
     .onConflict([UserTable.default.columns.id.name])
     .ignore();
